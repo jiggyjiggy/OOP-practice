02442886de6a3dbbd893bc592f62ea6f3398df74
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-02 16:04:37 +0900

    feat(Calculator#calculate): Add times and division

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,9 +1,14 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * 구현 사항
@@ -14,24 +19,24 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class CalculatorTest {
 	
-	@DisplayName("덧셈 연산을 수행한다")
-	@Test
-	void additionTest() {
+	@DisplayName("사칙 연산을 수행한다")
+	@ParameterizedTest()
+	@MethodSource("formulaAndResult")
+	void additionTest(int operand1, String operator, int operand2, int result) {
 		// Given
 		Calculator calculator = new Calculator();
-		int result = calculator.calculate(1, "+", 2);
+		int actual = calculator.calculate(operand1, operator, operand2);
 		
 		// When & Then
-		assertThat(result).isEqualTo(3);
+		assertThat(actual).isEqualTo(result);
 	}
-	@DisplayName("뺄셈 연산을 수행한다")
-	@Test
-	void subtractionTest() {
-		// Given
-		Calculator calculator = new Calculator();
-		int result = calculator.calculate(1, "-", 2);
-		
-		// When & Then
-		assertThat(result).isEqualTo(-1);
+	
+	private static Stream<Arguments> formulaAndResult() {
+		return Stream.of(
+			arguments(1, "+", 2, 3),
+			arguments(1, "-", 2, -1),
+			arguments(1, "*", 2, 2),
+			arguments(4, "/", 2, 2)
+		);
 	}
 }
