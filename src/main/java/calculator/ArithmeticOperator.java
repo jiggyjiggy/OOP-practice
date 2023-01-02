f0c34861192736908c3ed694821474bcaa5a4415
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-02 16:32:00 +0900

    refactor: Delegate arithmetic calculate

---
+++ src/main/java/calculator/ArithmeticOperator.java
@@ -0,0 +1,51 @@
+package calculator;
+
+import java.util.Arrays;
+
+public enum ArithmeticOperator {
+	ADDITION("+") {
+		@Override
+		public int arithmeticCalculate(int operand1, int operand2) {
+			return operand1 + operand2;
+		}
+	},
+	SUBTRACTION("-") {
+		@Override
+		public int arithmeticCalculate(int operand1, int operand2) {
+			return operand1 - operand2;
+		}
+	},
+	MULTIPLICATION("*") {
+		@Override
+		public int arithmeticCalculate(int operand1, int operand2) {
+			return operand1 * operand2;
+		}
+	},
+	DIVISION("/") {
+		@Override
+		public int arithmeticCalculate(int operand1, int operand2) {
+			return operand1 / operand2;
+		}
+	};
+	
+	private final String operator;
+	
+	ArithmeticOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	public abstract int arithmeticCalculate(int operand1, int operand2);
+	
+	public static int calculate(int operand1, String operator, int operand2) {
+		ArithmeticOperator arithmeticOperator = findArithmeticOperator(operator);
+		
+		return arithmeticOperator.arithmeticCalculate(operand1, operand2);
+	}
+	
+	private static ArithmeticOperator findArithmeticOperator(String operator) {
+		return Arrays.stream(values())
+			.filter(v -> v.operator.equals(operator))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("지원하지 않는 연산자입니다."));
+	}
+}
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,18 +2,6 @@ package calculator;
 
 public class Calculator {
 	public int calculate(int operand1, String operator, int operand2) {
-		if ("+".equals(operator)) {
-			return operand1 + operand2;
-		}
-		if ("-".equals(operator)) {
-			return operand1 - operand2;
-		}
-		if ("*".equals(operator)) {
-			return operand1 * operand2;
-		}
-		if ("/".equals(operator)) {
-			return operand1 / operand2;
-		}
-		return 0;
+		return ArithmeticOperator.calculate(operand1, operator, operand2);
 	}
 }
